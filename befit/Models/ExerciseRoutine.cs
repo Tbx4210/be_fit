b35766eae9b4b39f7a9673ef4353baad82c817ea
parth14899 <parth14899james@gmail.com>
2025-04-03 16:17:02 -0400

    Created ExerciseRoutineModel class to store exercise details such as name, sets, reps, duration, and difficulty.

---
+++ befit/Models/ExerciseRoutine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BE_FIT.Models
+{
+    
+    public class ExerciseRoutineModel
+    {
+        public string ExerciseName { get; set; }  
+        public int Sets { get; set; }             
+        public int Reps { get; set; }            
+        public double Duration { get; set; }      
+        public string Difficulty { get; set; }    
+        public string Notes { get; set; }         
+    }
+}
