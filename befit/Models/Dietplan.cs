b2a9e7f2b3c2b077d50a22ed4793bec0cc4a45af
parth14899 <parth14899james@gmail.com>
2025-04-03 16:19:02 -0400

    Created DietPlanModel class to store meal-related data such as meal name, calories, protein, carbs, and fat.

---
+++ befit/Models/Dietplan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace BE_FIT.Models
+{
+   
+    public class DietPlanModel
+    {
+        public string MealName { get; set; }      
+        public string MealType { get; set; }     
+        public double Calories { get; set; }     
+        public double Protein { get; set; }       
+        public double Carbohydrates { get; set; } 
+        public double Fat { get; set; }           
+
+        
+    }
+}
+
