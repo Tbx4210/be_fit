8299e33c46fec240813c6586e1d674a736452ec9
parth14899 <parth14899james@gmail.com>
2025-04-03 16:14:30 -0400

    Created UserModel class to store user information for the BE-FIT app, including name, email, age, gender, and weight.

---
+++ befit/Models/user.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_FIT.Models
+{
+    
+    public class UserModel
+    {
+        public string UserId { get; set; } 
+        public string Name { get; set; }   
+        public string Email { get; set; }   
+        public int Age { get; set; }        
+        public string Gender { get; set; }  
+        public double Weight { get; set; }
+    }
+}
